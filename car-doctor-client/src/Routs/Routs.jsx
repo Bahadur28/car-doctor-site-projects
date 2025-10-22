af02c22dcfc97ec79252709bbd248874633047e1
Md Sabbir Bahadur <164058840+Bahadur28@users.noreply.github.com>
2025-10-22 21:26:29 +0600

    Add files via upload

---
+++ car-doctor-client/src/Routs/Routs.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Page/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import CheckOut from "../Pages/CheckOut/CheckOut";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: 'login',
+            element: <Login></Login>
+        },
+        {
+            path: 'signup',
+            element: <SignUp></SignUp>
+        },
+        {
+          path: 'checkout/:id',
+          element: <CheckOut></CheckOut>
+        }
+    ]
+  },
+]);
+
+export default router;
---
+++ car-doctor-client/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routs/Routs.jsx';
+import AuthProviders from './providers/AuthProviders.jsx';
+
+createRoot(document.getElementById('root')).render(
+ <div className='max-w-7xl mx-auto'>
+   <StrictMode>
+    <AuthProviders>
+     <RouterProvider router={router} />
+    </AuthProviders>
+  </StrictMode>,
+ </div>
+)
